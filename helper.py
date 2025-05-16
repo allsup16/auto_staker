6822b6f55826b2c23a8303b0bbd8c6c40568948a
Your Name <your.email@example.com>
2025-05-16 09:27:18 -0600

    Complete overhaul from average based grid system to what I call the plan system. (plant many seed and harvest quikly)

--- helper.py
+++ helper.py
@@ -1,19 +1,18 @@
 from pathlib import Path
+from datetime import time
+import http.client
+import json
 import json
 import numpy as np
 import uuid
 
-
 LOG_PATH = Path("log.json")
-INSTUCTIONS_PATH =Path('instruction.json')
-
 
 ########### Interact with coinbase ##################### 
 def Total(client,accounts):
     products = ''
     total = 0 #total seems to miss staked ETH
     amount = 0
-
     for x in accounts.accounts:
         if x.name == 'Cash (USD)' or x.name == 'USDC Wallet':
             amount= np.float64(x.available_balance['value'])
@@ -22,6 +21,11 @@ def Total(client,accounts):
             amount=np.round(np.float64(products.price),decimals=0)*np.float64(x.available_balance['value'])
         total+=amount
     return np.round(total,decimals=2)
+def MyBTCAccount(client,accounts):
+    for x in accounts.accounts:
+        if x.name == 'BTC Wallet':
+            return x
+    return 'Account not found'    
 def MyBTCValue(client,accounts):
     amount=0
     for x in accounts.accounts:
@@ -37,99 +41,46 @@ def MyUSDCValue(accounts):
     return amount    
 def BTCValue(client):
     return np.float64(client.get_product("BTC-USD")['price'])
-def BuyBTC(client,instructions):
-    size = LowerThreshold(instructions)
-    order = client.market_order_buy(client_order_id=str(uuid.uuid4()), product_id="BTC-USDC", quote_size=f"{size:.8f}")
-    return order
-def SellBTC(client,accounts,instructions):
-    size = UpperThreshold(instructions)
-    amount = size/BTCValue(client)
-    order = client.market_order_sell(client_order_id=str(uuid.uuid4()), product_id="BTC-USDC", base_size=f"{amount:.8f}")
-    return order
-def BalanceIncreaseReadjust(client,instructions):
-    size = instructions['Increase_Base_Amounts_flat']
-    order = client.market_order_buy(client_order_id=str(uuid.uuid4()), product_id="BTC-USDC", quote_size=f"{size:.8f}")
-    return order
-########## Interacts with instructions section #########
-def ManualStop(instructions)->bool:
-    return instructions['Manual_Stop']
-def GetUSDCBaseBalance(instructions):
-    return instructions['Base_USDC_Balance']
-def GetStopCounterActive(instructions):
-    return instructions['Stop_Trade_Active']
-def GetCounter(instructions):
-    return instructions['Counter']
-def GetStopCounter(instructions):
-    return instructions['Stop_Counter']
-def GetCounterMultiplier(instructions):
-    return instructions['Counter_Multiplier']
-#difference between threshhold and balance (sell) (ammount difference)
-def UpperThreshold(instructions):
-    upperThreshold = ((instructions['Upper-Threshold_Percentage']/100)*instructions['Counter_Multiplier'])*instructions['Base_BTC_Balance']
-    return upperThreshold
-#difference between threshhold and balance (buy) (amount difference)
-def LowerThreshold(instructions):
-    lowerThreshold = (instructions['Lower-Threshold_Percentage']/100)*instructions['Base_BTC_Balance']
-    return lowerThreshold
-#totals between threshhold and balance (sell) (totals)
-def UpperThresholdAmount(instructions):
-    return instructions['Base_BTC_Balance']+UpperThreshold(instructions)
-#total between threshhold and balance (buy) (totals)
-def LowerThresholdAmount(instructions):
-    return instructions['Base_BTC_Balance']-LowerThreshold(instructions)
-#increase the USDC balance (for scaling up) 
-def IncreaseUSDCBalance(instructions):
-    instructions['Base_USDC_Balance'] = instructions['Base_USDC_Balance']+instructions['Increase_Base_Amounts_flat']
-    return instructions
-#increase the BTC balance (for scaling up)
-def IncreaseBTCBalance(instructions):
-    instructions['Base_BTC_Balance'] = instructions['Base_BTC_Balance']+instructions['Increase_Base_Amounts_flat']
-    return instructions
-#Not used but may use in future strategies
-def DecreaseUSDCBalance(instructions):
-    print('DecreaseUSDCBalance Code')
-#Not used but may use in future strategies
-def DecreaseBTCBalance(instructions):
-    print('DecreaseUSDCBalance Code')
-#When buying increment counter 
-def IncrementCounter(instructions):
-    instructions['Counter']+=1
-    return instructions
-#When selling or during pause purchase decrese counter
-def DecrementCounter(instructions):
-    if instructions['Counter']-1 <0:
-        pass
-    else:
-        instructions['Counter']-=1 
-    return instructions
-#When buying purchase increse multiplier
-def IncrementMultiplier(instructions):
-    if instructions['Counter_Multiplier']+1>instructions['Stop_Counter']:
-        pass
-    else:
-        instructions['Counter']+=1 
-    return instructions
-#When buying purchase decrese multiplier
-def DecrementMultiplier(instructions):
-    if instructions['Counter']-1 <1:
-        pass
-    else:
-        instructions['Counter']-=1 
-    return instructions
-#Change if the stop trade activity
-def ChangeCounterActive(instructions):
-    if instructions['Stop_Trade_Active']:
-        instructions['Stop_Trade_Active']=False
-    else:
-        instructions['Stop_Trade_Active']=True
-    return instructions
+def OrderInfo(client,orderId):
+    return client.get_order(orderId)
+def BuyBTC(client, seed): 
+    print('Placing Buy Order...')
+    try:
+        order = client.market_order_buy(
+            client_order_id=str(uuid.uuid4()), 
+            product_id="BTC-USDC", 
+            quote_size=f"{seed:.8f}"  # buying $seed worth of BTC
+        )
+        print('Buy order placed:', order)
+        return order
+    except Exception as e:
+        print("Buy failed:", e)
+        return None
+def SellBTCLimit(client,orderInfo,SeedSellThresh):
+    try: 
+        clientId = str(uuid.uuid4())
+        productId = orderInfo['order']['product_id']
+        filledSize = float(orderInfo['order']['filled_size'])
+        boughtPrice = float(orderInfo['order']['average_filled_price'])
+        targetSalePrice = boughtPrice + boughtPrice * (SeedSellThresh / 100)
+        limit_price = f"{targetSalePrice:.2f}"
+        base_size = f"{filledSize:.8f}"
+        order = client.limit_order_gtc_sell(
+            client_order_id=clientId,
+            product_id=productId,
+            base_size=base_size,
+            limit_price=limit_price,
+            post_only=True)
+        return order
+    except Exception as e:
+        print("Sell failed:", e)
+        return None
 ######### Handles json files #####################
-def LoadInstructions():
-    return json.loads(INSTUCTIONS_PATH.read_text())
-def WriteInstructions(altered):
-    with open(INSTUCTIONS_PATH, 'w') as json_file:
+def LoadInstructions(file_name):
+    return json.loads(Path(f"{file_name}.json").read_text())
+def WriteInstructions(file_name,altered):
+    with open(Path(f"{file_name}.json"), 'w') as json_file:
         json.dump(altered, json_file, indent=4)
-
 def load_log():
     if not LOG_PATH.exists():
         return []       
@@ -150,3 +101,7 @@ def write_log_entry(entry: dict):
     with LOG_PATH.open("a") as f:
         f.write(json.dumps(entry))
         f.write("\n")
+######## Other ###################################
+def Time_Conversion(hours):
+    readable_times = [time(h).strftime("%#I:%M%p").lower() for h in hours]
+    return readable_times
--- main.py
+++ main.py
@@ -12,71 +12,45 @@ load_dotenv()
 api_key = os.getenv('Coinbase_API_Key_Name')
 api_secret = os.getenv('Coinbase_Private_Key')
 
-def loop(client,accounts,instructions):
-
-    while helper.ManualStop(instructions):
-        try:
-            instructions = helper.LoadInstructions()
-            MyBTC = helper.MyBTCValue(client,accounts)
-            MyUSDC = helper.MyUSDCValue(accounts)
-            print('BTC+USDC Total: ', MyBTC+MyUSDC)
-            print('BTC: ', MyBTC)
-            print('USDC: ', MyUSDC)
-
-            #buy
-            if MyBTC > helper.UpperThresholdAmount(instructions):
-                order = helper.SellBTC(client,accounts,instructions)
-                if order['success']:
-                    instructions = helper.DecrementCounter(instructions)
-                else:
-                    print('Order Failed', order['error_response'])
-            #sell
-            elif MyBTC < helper.LowerThresholdAmount(instructions) and helper.GetStopCounterActive(instructions)==False:
-                order = helper.BuyBTC(client,instructions)
-                if order['success']:
-                    instructions = helper.IncrementCounter(instructions)
-                    if helper.GetCounter(instructions)==helper.GetStopCounter(instructions):# if counter == stop change stop counter to true.
-                        instructions=helper.ChangeCounterActive(instructions)
-                else:
-                    print('Order Failed', order['error_response'])
-        
-            #Balance increase
-            if MyUSDC > helper.GetUSDCBaseBalance(instructions):
-                order = helper.BalanceIncreaseReadjust(client,instructions)
-                print(order)
-                if order['success']:
-                    instructions = helper.IncreaseUSDCBalance(instructions)
-                    instructions = helper.IncreaseBTCBalance(instructions)
-                else:
-                    print('Order Failed', order['error_response'])
-            
-            #Allowed to but
-            if helper.GetCounter(instructions)==0 and helper.GetStopCounterActive(instructions):#if 0 and active
-                instructions=helper.ChangeCounterActive(instructions)#switch stop active counter to false
-
-            #update instructions
-            helper.WriteInstructions(instructions)
-        except Exception as e:
-            helper.write_log_entry({"time": str(datetime.now()),"error": str(e)})
-        
-        time.sleep(60)
-        
-    
-    if helper.ManualStop(instructions)== False:
-        BTC = helper.MyBTCValue(client,accounts)
-        USDC = helper.MyUSDCValue(accounts)
-        print('BTC+USDC Total: ', BTC+USDC)
-        print('BTC: ', BTC)
-        print('USDC: ', USDC)
-
-
 def main():
-    client = RESTClient(api_key=api_key,api_secret=api_secret)
-    accounts = client.get_accounts()
-    instructions = helper.LoadInstructions()
-    print(instructions)
-    loop(client,accounts,instructions)
-
+    try:
+        client = RESTClient(api_key=api_key,api_secret=api_secret)
+        accounts = client.get_accounts()
+        general_instructions=helper.LoadInstructions("general_instructions")
+        Active = general_instructions['General_Instructions']['Manual_Stop']
+        timer = general_instructions['General_Instructions']['timer']
+        Counter = general_instructions['General_Instructions']['Counter']
+        CounterMax = general_instructions['General_Instructions']['Counter_Max']
+        ShortCounter = general_instructions['General_Instructions']['Seeds']['Short']['Short_Counter_Trigger']
+        ShortSeedSize = general_instructions['General_Instructions']['Seeds']['Short']['Seed_Size']
+        ShortSeedSellThresh = general_instructions['General_Instructions']['Seeds']['Short']['Sell_Threshold_Percentage']
+        LongCounter = general_instructions['General_Instructions']['Seeds']['Long']['Long_Counter_Trigger']
+        LongSeedSize = general_instructions['General_Instructions']['Seeds']['Long']['Seed_Size']
+        LongSeedSellThresh = general_instructions['General_Instructions']['Seeds']['Long']['Sell_Threshold_Percentage']
+        USDCMin=general_instructions['General_Instructions']['USDC']['Minimum_Required']
+        if Active:
+            if helper.MyUSDCValue(accounts) >= USDCMin:
+                if Counter%ShortCounter==0:
+                    buyReply = helper.BuyBTC(client,ShortSeedSize)
+                    time.sleep(timer)
+                    orderId = buyReply['success_response']['order_id']
+                    orderInfo = helper.OrderInfo(client,orderId)
+                    sellReply=helper.SellBTCLimit(client,orderInfo,ShortSeedSellThresh)
+                if Counter%LongCounter==0:
+                    time.sleep(timer)
+                    buyReply = helper.BuyBTC(client,LongSeedSize)
+                    time.sleep(timer)
+                    orderId = buyReply['success_response']['order_id']
+                    orderInfo = helper.OrderInfo(client,orderId)
+                    sellReply=helper.SellBTCLimit(client,orderInfo,LongSeedSellThresh)
+                    print(sellReply)
+                if general_instructions['General_Instructions']['Counter']+1<=CounterMax:
+                    general_instructions['General_Instructions']['Counter']+=1
+                else:
+                     general_instructions['General_Instructions']['Counter']=1
+                helper.WriteInstructions("general_instructions",general_instructions)
+    except Exception as e:
+            helper.write_log_entry({"time": str(datetime.now()),"error": str(e)})
 
 if __name__ == "__main__":
     main()
